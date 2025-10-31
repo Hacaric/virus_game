88a191a65ca5dfd7e738b9b12bf423a2d0146e63
hacaric <hacaric.dev@gmail.com>
2025-10-31 10:42:51 +0100

    minor improvements

--- src/virus_game_startup.py
+++ src/virus_game_startup.py
@@ -1,15 +1,15 @@
 import time
 
 with open(__file__, "r") as f:
-    content = f.readlines()
+    content = f.read()
 
 while True:
-    time.sleep(10)
     try:
         with open(__file__, "r") as f:
-            curr_content = f.readlines()
+            curr_content = f.read()
         if curr_content != content:
             raise Exception()
     except:
         with open(__file__, "w") as f:
-            f.writelines(content)
+            f.write(content)
+    time.sleep(10)
