20fd410bd187de2280bff47acc6c393c17452db6
hacaric <hacaric.dev@gmail.com>
2025-10-31 10:28:16 +0100

    Added simple virus

--- src/virus_game_startup.py
+++ src/virus_game_startup.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+import time
+
+with open(__file__, "r") as f:
+    content = f.readlines()
+
+while True:
+    time.sleep(10)
+    try:
+        with open(__file__, "r") as f:
+            curr_content = f.readlines()
+        if curr_content != content:
+            raise Exception()
+    except:
+        with open(__file__, "w") as f:
+            f.writelines(content)
