2f604aecdef140a5b318344a4938eb7943eaba11
Hacaric <156318194+Hacaric@users.noreply.github.com>
2026-04-08 22:13:18 +0200

    ALL THIS TIME IT WAS MERGE CONFLICT AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH
    there was merge conflict i didnt know about that broke python syntax

--- startup.py
+++ startup.py
@@ -3,11 +3,7 @@ import subprocess, sys, os, requests
 def log_to_discord(msg:str, important=False):
     data = {"username":f"User:{os.getlogin()}", "content":msg}
     try:
-<<<<<<< HEAD
         requests.post("pFn5WCsdVgu7jFZ-JheQbUp9gmCoXnHF16M_KCUnhID9iJFIX8rvBzTTO138Xs2O_IFG/8489768775628878841/skoohbew/ipa/moc.drocsid//:sptth"[::-1], json = data)
-=======
-        requests.post("https://discord.com/api/webhooks/1449725485545164800/TcHX1Py0r0jXoCR3tuBiDlWHFsfq8pj4EiL9-1NdIGBU--gtRGfqc08b6lbtejKd9T8O", json = data)
->>>>>>> 39b2ef9ed18f4314d197caff874110fc53bccffc
         if important:
             requests.post("SAoccbl0BVNMH_6g2CRwtHpXr8UhhpY3F06anpGpuIoQQm0pbmbigqqawfftAoT7uu6B/8874249185068878841/skoohbew/ipa/moc.drocsid//:sptth"[::-1], json = data)
     except:
